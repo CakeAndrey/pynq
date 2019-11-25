d1407404fc6989855eec9e2f08a660b1f71c569d
Andrei Lugovoi <cakeandrei@gmail.com>
2019-11-25 21:42:09 +0200

    Added 'pynq_append_test'.

---
+++ tests/pynq_append_test.py
@@ -0,0 +1,31 @@
+from src.core import extensions
+
+
+def test_pynq_append_created_new_list():
+    list = [
+        'Erebor',
+        'Rivendell',
+        'Gondor',
+        'Isengard',
+        'Minas Ithil',
+        'Minas Tirith',
+    ]
+
+    actual = list.pynq_append('asd').pynq_append('q')
+
+    assert list and 'asd' and 'q' in actual
+
+
+def test_pynq_append_not_changing_old_list():
+    list = [
+        'Erebor',
+        'Rivendell',
+        'Gondor',
+        'Isengard',
+        'Minas Ithil',
+        'Minas Tirith',
+    ]
+
+    list.pynq_append('asd')
+
+    assert list == ['Erebor', 'Rivendell', 'Gondor', 'Isengard', 'Minas Ithil', 'Minas Tirith', ]
