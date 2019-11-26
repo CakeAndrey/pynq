4958ff1eca8630c9c7ba412517702759f86c0cd9
Andrei Lugovoi <cakeandrei@gmail.com>
2019-11-26 09:04:43 +0200

    Added tests for 'select'.

---
+++ tests/select_test.py
@@ -0,0 +1,38 @@
+import src.core.extensions
+
+
+def test_select_returns_values_as_strings():
+    list = [
+        123,
+        True,
+        None,
+        'Aaa',
+        {}
+    ]
+
+    actual = list.select(lambda x: str(x))
+    expected = [
+        str(123),
+        str(True),
+        str(None),
+        str('Aaa'),
+        str({})
+    ]
+
+    assert actual == expected
+
+
+def test_select_not_changing_old_list():
+    default = [
+        123,
+        True,
+        None,
+        'Aaa',
+        {}
+    ]
+
+    list = default.copy()
+
+    list.select(lambda x: str(x))
+
+    assert list == default
